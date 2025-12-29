6d9c914f0b4ace0f2a68e0d8365833ae85b6f97d
emma-ola <iam_newton@outlook.com>
2025-12-29 16:46:33 -0500

    feat(infra): Add variables for network configuration in Terraform module, including project ID, VPC settings, and subnet definitions

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,5 +1,39 @@
-variable "instance_type" {
-  type        = string                     # The type of the variable, in this case a string
-  default     = "t2.micro"                 # Default value for the variable
-  description = "The type of EC2 instance" # Description of what this variable represents
+variable "project_id" {
+  description = "GCP project ID where the network will be created."
+  type        = string
+}
+
+variable "network_name" {
+  description = "Name of the VPC network."
+  type        = string
+}
+
+variable "routing_mode" {
+  description = "VPC routing mode: REGIONAL or GLOBAL."
+  type        = string
+  default     = "REGIONAL"
+
+  validation {
+    condition     = contains(["REGIONAL", "GLOBAL"], var.routing_mode)
+    error_message = "routing_mode must be REGIONAL or GLOBAL."
+  }
+}
+
+variable "subnets" {
+  description = <<EOT
+Map of subnets to create. Key is an identifier (e.g., 'apps-us-central1').
+Each subnet supports optional secondary ranges for use cases like GKE.
+EOT
+
+  type = map(object({
+    name                     = string
+    region                   = string
+    ip_cidr_range            = string
+    private_ip_google_access = optional(bool, true)
+
+    secondary_ranges = optional(map(object({
+      range_name    = string
+      ip_cidr_range = string
+    })), {})
+  }))
 }
