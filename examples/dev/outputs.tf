f69bce907e35dc953fd86af125c74de2b18214e5
emma-ola <iam_newton@outlook.com>
2025-12-29 18:06:57 -0500

    feat(examples): Add network-related outputs to dev environment example

--- examples/dev/outputs.tf
+++ examples/dev/outputs.tf
@@ -5,3 +5,23 @@ output "project_id" {
 output "enabled_apis" {
   value = module.project.enabled_apis
 }
+
+output "network_name" {
+  value       = module.network.network_name
+}
+
+output "network_id" {
+  value       = module.network.network_id
+}
+
+output "subnet_names" {
+  value       = module.network.subnet_names
+}
+
+output "subnet_self_links" {
+  value       = module.network.subnet_self_links
+}
+
+output "subnet_secondary_ranges" {
+  value = module.network.subnet_secondary_ranges
+}
