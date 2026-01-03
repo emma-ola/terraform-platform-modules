d602910b4f2ee6acc0e9f480d14879b8274dfbf3
emma-ola <iam_newton@outlook.com>
2026-01-03 11:39:42 -0500

    feat(gke): configure GKE dev example with subnets, secondary IP ranges, and cluster setup

--- examples/gke-dev/main.tf
+++ examples/gke-dev/main.tf
@@ -16,13 +16,54 @@ module "network" {
 
   project_id   = module.project.project_id
   network_name = "platform-gke-vpc"
-  subnets      = {}
+  subnets = {
+    gke_us_central1 = {
+      name          = "gke-us-central1"
+      region        = var.region
+      ip_cidr_range = "10.10.0.0/16"
+      flow_logs = {
+        enabled              = true
+        aggregation_interval = "INTERVAL_10_MIN"
+      }
+      secondary_ranges = {
+        pods = {
+          range_name    = "pods"
+          ip_cidr_range = "10.20.1.0/24"
+        }
+        svc = {
+          range_name    = "services"
+          ip_cidr_range = "10.30.1.0/24"
+        }
+      }
+    }
+  }
   nat = {
     enabled = false
     regions = {
       (var.region) = {
         source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_PRIMARY_IP_RANGES"
       }
+    }
+  }
+}
+
+module "gke" {
+  source = "../../modules/gke"
+
+  project_id           = var.project_id
+  name                 = "platform-dev"
+  location             = var.region
+  regional             = true
+  network_self_link    = module.network.network_id
+  subnetwork_self_link = module.network.subnet_self_links["gke_us_central1"]
+  ip_range_pods        = "pods"
+  ip_range_services    = "svc"
+  node_pools = {
+    default = {
+      machine_type = "e2-standard-4"
+      min_count    = 1
+      max_count    = 3
+      disk_size_gb = 50
     }
   }
 }
