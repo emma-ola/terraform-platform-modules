b15782191f9c72c9f930f6dc5e99a5c5e186bbbf
emma-ola <iam_newton@outlook.com>
2026-01-17 12:22:45 -0500

    feat(gke-dev): add outputs for project, network, and GKE module attributes

---
+++ examples/gke-dev/outputs.tf
@@ -0,0 +1,35 @@
+output "project_id" {
+  value = module.project.project_id
+}
+
+output "enabled_apis" {
+  value = module.project.enabled_apis
+}
+
+output "network_name" {
+  value = module.network.network_name
+}
+
+output "network_id" {
+  value = module.network.network_id
+}
+
+output "subnet_names" {
+  value = module.network.subnet_names
+}
+
+output "subnet_self_links" {
+  value = module.network.subnet_self_links
+}
+
+output "subnet_secondary_ranges" {
+  value = module.network.subnet_secondary_ranges
+}
+
+output "endpoint" {
+  value = module.gke.endpoint
+}
+
+output "node_service_account_email" {
+  value = module.gke.node_service_account_email
+}
